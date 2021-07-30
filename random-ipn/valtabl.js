c8df124e8ddf6aa1358f2452f2cc683abd27a61f
Bogdan Artyushenko <Bogdan.Artyushenko@gmail.com>
2021-07-30 11:25:25 +0300

    added lat

--- random-ipn/valtabl.js
+++ random-ipn/valtabl.js
@@ -3,6 +3,8 @@
 function get_random_state() {
   let dob = new Date((Date.now() - 18 * 365 * 24 * 60 * 60 * 1000) * Math.random());
   let ipn = make_taxcode_fiz(dob);
+  let idx_name = Math.floor(Math.random() * names["ім'я"][get_sex(ipn)].length);
+  let idx_lastname = Math.floor(Math.random() * names["прізвище"][get_sex(ipn)].length);
   return {
     "bank-iban": make_iban(),
     "bank-mastercard": '5' + makernd('12345', 1) + make_digits(16 - 2),
@@ -21,9 +23,11 @@ function get_random_state() {
     "contactna-telefon": "+380" + make_digits(9),
     "zagalna-data-narodgenia": dob.toISOString().substr(0, 10),
     "zagalna-demindex": make_dem_index(dob),
-    "zagalna-firstname-ukr": rnd_arr_element(names["ім'я"][get_sex(ipn)]),
+    "zagalna-firstname-ukr": names["ім'я"][get_sex(ipn)][idx_name],
+    "zagalna-firstname-lat": names["ім'я-lat"][get_sex(ipn)][idx_name],
     "zagalna-secondname-ukr": rnd_arr_element(names["по-батькові"][get_sex(ipn)]),
-    "zagalna-lastname-ukr": rnd_arr_element(names["прізвище"][get_sex(ipn)]),
+    "zagalna-lastname-ukr": names["прізвище"][get_sex(ipn)][idx_lastname],
+    "zagalna-lastname-lat": names["прізвище-lat"][get_sex(ipn)][idx_lastname],
     "zagalna-vaga": 50 + Math.round(100 * Math.random()),
     "zagalna-stat": get_sex(ipn),
     "zagalna-zrist": 150 + Math.round(50 * Math.random()),
@@ -66,11 +70,15 @@ class ValuesTable extends React.Component {
       className: "zagalna-demindex"
     }, this.state["zagalna-demindex"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0456\u043C'\u044F"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "zagalna-firstname-ukr"
-    }, this.state["zagalna-firstname-ukr"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u043F\u043E \u0431\u0430\u0442\u044C\u043A\u043E\u0432\u0456"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
+    }, this.state["zagalna-firstname-ukr"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0456\u043C'\u044F (\u043B\u0430\u0442\u0438\u043D\u0438\u0446\u044F)"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
+      className: "zagalna-firstname-ukr"
+    }, this.state["zagalna-firstname-lat"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u043F\u043E \u0431\u0430\u0442\u044C\u043A\u043E\u0432\u0456"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "zagalna-secondname-ukr"
     }, this.state["zagalna-secondname-ukr"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u043F\u0440\u0456\u0437\u0432\u0438\u0449\u0435"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "zagalna-lastname-ukr"
-    }, this.state["zagalna-lastname-ukr"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0434\u0430\u0442\u0430 \u043D\u0430\u0440\u043E\u0434\u0436\u0435\u043D\u043D\u044F"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
+    }, this.state["zagalna-lastname-ukr"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u043F\u0440\u0456\u0437\u0432\u0438\u0449\u0435 (\u043B\u0430\u0442\u0438\u043D\u0438\u0446\u044F)"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
+      className: "zagalna-lastname-ukr"
+    }, this.state["zagalna-lastname-lat"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0434\u0430\u0442\u0430 \u043D\u0430\u0440\u043E\u0434\u0436\u0435\u043D\u043D\u044F"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "zagalna-data-narodgenia"
     }, this.state["zagalna-data-narodgenia"]))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, "\u0437\u0430\u0433\u0430\u043B\u044C\u043D\u0430 \u0456\u043D\u0444\u043E"), /*#__PURE__*/React.createElement("td", null, "\u0441\u0442\u0430\u0442\u044C"), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement("span", {
       className: "zagalna-stat"
