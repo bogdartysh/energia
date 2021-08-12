09e2694dcb127fa163bfb33196577df1e272d2c8
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2021-08-12 12:27:28 +0300

    Update makernd.js

--- random-ipn/babel/makernd.js
+++ random-ipn/babel/makernd.js
@@ -140,5 +140,5 @@ function make_le_name() {
 }
 
 function toUkrDateTime(date) {
-  return date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " " + date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds();
+  return date.getDate() + "." + (1 + date.getMonth()) + "." + date.getFullYear() + " " + date.getHours() +":"+ date.getMinutes() +":"+ date.getSeconds();
 }
