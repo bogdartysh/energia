e47ba5a8c12151e3a0e42401cac4188a4e151512
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2021-08-12 12:18:34 +0300

    Update valtabl-pr.js

--- random-ipn/babel/valtabl-pr.js
+++ random-ipn/babel/valtabl-pr.js
@@ -2,7 +2,10 @@
 function          getRandomState() {
 
                 return {
-                  "land-kadastr" : make_digits(12) + ':' +make_digits(3) +':' + make_digits(4)
+                  "land-kadastr" : make_digits(12) + ':' +make_digits(3) +':' + make_digits(4),
+                   "neruhome-reiestr-nomer": make_digits(13),
+                    "neruhome-nomer-vlasnosti": make_digits(9),
+                    "neruhome-data-vlasnosti": toUkrDateTime(new Date(Math.round(Date.now() - 10*365*24*60*60*1000*Math.random())))
                 };
            };
 
@@ -41,6 +44,21 @@ function          getRandomState() {
                             <td>земельна ділянка</td>
                             <td>кадастровий номер</td>
                             <td><span className="land-kadastr">{this.state["land-kadastr"]}</span></td>
+                        </tr>
+                        <tr>
+                            <td>нерухоме майно</td>
+                            <td>реєстраційний номер обєкта нерухомого майна</td>
+                            <td><span className="neruhome-reiestr-nomer">{this.state["neruhome-reiestr-nomer"]}</span></td>
+                        </tr>
+                         <tr>
+                            <td>нерухоме майно</td>
+                            <td>номер запису про право власності</td>
+                            <td><span className="neruhome-nomer-vlasnosti">{this.state["neruhome-nomer-vlasnosti"]}</span></td>
+                        </tr>
+                         <tr>
+                            <td>нерухоме майно</td>
+                            <td>дата, час державної реєстрації</td>
+                            <td><span className="neruhome-data-vlasnosti">{this.state["neruhome-data-vlasnosti"]}</span></td>
                         </tr>
                         </tbody>
                     </table>
